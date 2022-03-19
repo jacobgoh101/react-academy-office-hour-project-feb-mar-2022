54c6fcaff04a2bceb349ea8876119a2a4ec958ae
Jacob Goh <jacobgoh101@gmail.com>
2022-03-19 23:19:55 +0800

    feat(routing): Setup client side routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
-import { Dashboard } from './components/layouts/dashboard';
+import { Route, Switch } from 'wouter';
+import { DashboardLayout } from './components/layouts/dashboard';
+import { LoginPage } from './pages/login';
+import { SignupPage } from './pages/sign-up';
 
 function App() {
-  return <Dashboard />;
+  return (
+    <Switch>
+      <Route path="/login">
+        <LoginPage />
+      </Route>
+      <Route path="/signup">
+        <SignupPage />
+      </Route>
+      <Route>
+        <DashboardLayout />
+      </Route>
+    </Switch>
+  );
 }
 
 export default App;
