08181b470a2791b6253ee64ea7cd5fce51ccff6e
Jacob Goh <jacobgoh101@gmail.com>
2022-03-22 11:24:49 +0800

    feat(api): Setup React Query for async operations

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,21 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import App from './App';
 import { ProvideAuth } from './context/auth.context';
 
+const queryClient = new QueryClient();
+
 ReactDOM.render(
   <React.StrictMode>
-    <ChakraProvider>
-      <ProvideAuth>
-        <App />
-      </ProvideAuth>
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider>
+        <ProvideAuth>
+          <App />
+        </ProvideAuth>
+      </ChakraProvider>{' '}
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
